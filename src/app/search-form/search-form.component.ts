68c49e87a57b9cc70c7ea1eeaadd59925bab29d3
kojwangbora <kojwangbora254@gmail.com>
2022-02-15 01:00:57 +0300

    modify searchform

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-form',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+  SearchName:string;
+  @Output() searchOutput = new EventEmitter<any>()
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.searchOutput.emit(this.SearchName);
+    this.SearchName = "";
   }
 
 }
