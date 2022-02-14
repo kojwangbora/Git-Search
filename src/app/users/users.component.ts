f5eeae762deda28da1d117b3775a91c02ac39769
kojwangbora <kojwangbora254@gmail.com>
2022-02-15 02:49:07 +0300

    modify usercomponent.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Repos } from '../repos';
+import { User } from '../user';
+import { UserserviceService } from '../userservice.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: User;
+  repo: Repos
 
-  constructor() { }
+  constructor( public myService: UserserviceService, private repoService: UserserviceService) { }
 
-  ngOnInit(): void {
+  searchs(searchName) {
+    this.myService.searchUser(searchName).then(
+      (success)=>{
+        this.user = this.myService.foundUser;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    );
+    this.repoService.getRepos(searchName).then(
+      (results)=>{
+        this.repo = this.repoService.allRepos
+        console.log(this.repo);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+
+  }
+
+  ngOnInit(){
+    this.searchs('kojwangbora')
   }
 
 }
